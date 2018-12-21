47a8a77194376356d61d2c1d8860b3a25b66052a
james rowbotham <james.rowbotham2@gmail.com>
2018-12-21 18:35:59 +0100

    connected DB

--- app.js
+++ app.js
@@ -2,33 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+const connectionString ='postgresql://admin:admin@localhost:5432/userPortfolio'
+const pool = new Pool({ connectionString: connectionString 
+});
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
+dotenv.config();
 
-app.get('/', (req, res) =>{
-    res.render('index')
-});
+app.get('/', (req, res) =>{res.render('index')});
 
-    const contact = [{
-        name: 'James',
-        company: 'TicketSwap',
-        email: 'james.rowbotham@hotmail.com',
-        phone: '0650406209',
-        description: 'lets work together'
-     }]
+app.post('/', (req, res, error) =>{
 
-app.post('/', (req, res) =>{  
-    const newContact = {
-        name: req.body.name,
-        company: req.body.company,
-        email: req.body.email,
-        phone: req.body.number,
-        description: req.body.description
-    }
-    contact.push(newContact)
-        res.send(contact)
+const newContact = [
+        req.body.name,
+        req.body.company,
+        req.body.email,
+        req.body.number,
+        req.body.description]
+
+const text = ('INSERT INTO users (name, company, email, number, description) values ($1, $2, $3, $4, $5)')
+
+pool.query(text, newContact ,(err, res) => {
+    if (err) {
+        console.log(err.stack)
+    } else {
+        console.log(res.rows[0]);       
+    }   
+})
+   res.redirect('/')
 })
 
 app.listen(port, () => console.log(`magic happening here ${port}!`))
+
+
+      
